00605338a4ce19b95758902a3f226c75157ad9b4
이태언 <mktears0609@gmail.com>
2022-12-10 16:07:50 +0900

    ADD: dart console setting

---
+++ first_learn_dart/bin/first_learn_dart.dart
@@ -0,0 +1,5 @@
+import 'package:first_learn_dart/first_learn_dart.dart' as first_learn_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${first_learn_dart.calculate()}!');
+}
---
+++ first_learn_dart/test/first_learn_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:first_learn_dart/first_learn_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
