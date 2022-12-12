353243f12a279c17c960c7c63d561687cc74a26b
이태언 <mktears0609@gmail.com>
2022-12-12 21:59:23 +0900

    TEST: var * implicit

--- first_learn_dart/lib/main.dart
+++ first_learn_dart/lib/main.dart
@@ -6,4 +6,7 @@ void main() {
   var name = "taeeon";
   // * implicit으로 타입지정도 가능하다
   String second = "taeeon";
+
+  // * 관습적으로 함수나 메소드 내부에서 local variable을 선언 할때는 var를 사용
+  // * class의 변수나 property를 선언할 때에는 타입을 지정해 준다.
 }
