6af6dbe26b42ac5b0cf69418922fecf8cf68e84e
이태언 <mktears0609@gmail.com>
2022-12-12 21:58:02 +0900

    test: variable

--- first_learn_dart/lib/main.dart
+++ first_learn_dart/lib/main.dart
@@ -1,6 +1,9 @@
 // * main에서 코드가 호출된다.
 
 void main() {
-  print("hello world");
-  print("test");
+  // * 변수를 선언 하고 타입은 따로 지정하지 않아도 다트가 자동으로 인식
+  // * 변수를 변경할때는 같은 타입으로만 가능;
+  var name = "taeeon";
+  // * implicit으로 타입지정도 가능하다
+  String second = "taeeon";
 }
