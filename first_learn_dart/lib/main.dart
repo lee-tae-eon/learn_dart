3ae678c0838c5ccc0c0f0fa30be2ed56eb86a0e3
이태언 <mktears0609@gmail.com>
2022-12-14 22:19:57 +0900

    dynamic type

--- first_learn_dart/lib/main.dart
+++ first_learn_dart/lib/main.dart
@@ -1,6 +1,8 @@
 // * main에서 코드가 호출된다.
 
 void main() {
+  // ------------- variable -------------------------------
+
   // * 변수를 선언 하고 타입은 따로 지정하지 않아도 다트가 자동으로 인식
   // * 변수를 변경할때는 같은 타입으로만 가능;
   var name = "taeeon";
@@ -9,4 +11,9 @@ void main() {
 
   // * 관습적으로 함수나 메소드 내부에서 local variable을 선언 할때는 var를 사용
   // * class의 변수나 property를 선언할 때에는 타입을 지정해 준다.
+
+  // ------------- dynamic type -------------------------------
+  dynamic name2;
+  // * dynamic type을 이용시 해당 타입에 대한 method를 이용하려면 type check가 이루어져야 한다.
+  if (name2 is String) {}
 }
