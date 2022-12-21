e0bd087f64663965b1dc4b0d2e5030e2ab847cfd
이태언 <mktears0609@gmail.com>
2022-12-21 23:18:30 +0900

    ADD: basict type

--- first_learn_dart/lib/datatype.dart
+++ first_learn_dart/lib/datatype.dart
@@ -1,3 +1,13 @@
 void main() {
-  print("test");
+  // Basic type ----------------------------------
+  // * dart의 모든 type은 object로 이루어져있다.
+  // * dart는 객체지향형 프로그래밍
+  String name = "taeeon";
+  bool alive = true;
+  int age = 12;
+  double money = 33.33;
+  // * num type은 integer 또는 double 타입 일 수 있다.
+  num x = 12;
+
+  // List type ------------------------
 }
