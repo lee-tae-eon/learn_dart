9b8b47a8c95d3eb9ae0c837aa00d19fac878a39c
이태언 <mktears0609@gmail.com>
2022-12-25 22:07:01 +0900

    implicit return function

--- first_learn_dart/lib/function_test.dart
+++ first_learn_dart/lib/function_test.dart
@@ -1,7 +1,10 @@
-String sayHello(String name) {
-  print("Hello $name come here");
-  return name;
-}
+// * 일반적인 function define
+// String sayHello(String name) {
+//   print("Hello $name come here");
+//   return name;
+// }
+// * implicit return
+String sayHello(String name) => name;
 
 void main() {
   sayHello("lee");
