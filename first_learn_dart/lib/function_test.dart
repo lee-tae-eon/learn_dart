85e4c0146cf28e64fbd51fb6c8152319be612f87
이태언 <mktears0609@gmail.com>
2022-12-26 22:22:53 +0900

    named parameter

--- first_learn_dart/lib/function_test.dart
+++ first_learn_dart/lib/function_test.dart
@@ -4,8 +4,17 @@
 //   return name;
 // }
 // * implicit return
-String sayHello(String name) => name;
+// String sayHello(String name) => name;
+
+// * named parameter
+// * nullable or default value 지정
+String sayHi({String name = "", int age = 0, String country = "korea"}) {
+  return "hello $name, are you $age? and from $country ?";
+}
 
 void main() {
-  sayHello("lee");
+  var test = sayHi(age: 12, name: "lee", country: "korea");
+  print(test);
+  // ---------- named parameters
+  // * dart function은 named parameter 를 provide > flutter
 }
