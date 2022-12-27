87154305c72e4d7d2c7a4b8e938f5af498cf5351
이태언 <mktears0609@gmail.com>
2022-12-27 22:29:13 +0900

    optioanl param

--- first_learn_dart/lib/function_test.dart
+++ first_learn_dart/lib/function_test.dart
@@ -15,7 +15,7 @@ String sayHi(
 }
 
 // * optional parameters
-String sayHello(String name, int age, String country) {
+String sayHello(String name, int age, [String? country = "korea"]) {
   return "Hello $name $age $country";
 }
 
@@ -27,5 +27,5 @@ void main() {
 
   // ------------- optional parameters
   // * 순서에 맞게 입력
-  sayHello("test", 23, "korea");
+  sayHello("test", 23);
 }
