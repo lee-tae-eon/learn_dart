81312201f6e8ae7dd6cbe5ad6bec829a098c5c0f
이태언 <mktears0609@gmail.com>
2023-01-04 21:22:23 +0900

    named constructor 2 && syntax test

--- first_learn_dart/lib/first_class.dart
+++ first_learn_dart/lib/first_class.dart
@@ -4,9 +4,8 @@ class Player {
   // late String name;
   // late int xp;
   final String name;
-  int xp;
+  int xp, age;
   String team;
-  int age;
 
   // * constructor 생성 -
   // * constructor 는 class의 이름과 같아야 한다.
@@ -24,18 +23,26 @@ class Player {
       required this.team,
       required this.age});
 
+  Player.createBluePlayer({required this.name, required this.age})
+      : team = "blue",
+        xp = 0;
+
+  Player.createRedPlayer({required this.name, required this.age})
+      : team = "red",
+        xp = 0;
+
   void sayHello() {
     // *안에 선언한 변수를 바로 쓸 수 있다 this를 써도 무방하지만 class method 내에서의 this는 사용하지 않는 것을 권고한다.
     // * method 안에 같은 이름의 변수가 있어서 어쩔 수 없이 this를 사용하는게 아닌이상 사용하지 않는 것이 권장사항이다.
-    print("hello $name are you $xp");
+    print("hello $age $name, you are $team team and  your team point is $xp");
   }
 }
 
 void main() {
   // * new를 써도 되지만 안써도 무방하다.
-  var player = Player(name: "taeeon", xp: 39, team: "red", age: 12);
+  var player = Player.createRedPlayer(name: "taeeon", age: 12);
   player.sayHello();
-  var player2 = Player(name: "woo", xp: 36, team: "blue", age: 32);
+  var player2 = Player.createBluePlayer(name: "woo", age: 32);
   player2.sayHello();
 
   // -------------------------- named constructor parameters
