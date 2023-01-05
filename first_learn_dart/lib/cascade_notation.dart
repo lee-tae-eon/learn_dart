909c8b408cc82f109a184f4b97790c39f6513f7c
이태언 <mktears0609@gmail.com>
2023-01-05 23:24:35 +0900

    cascade notation

--- first_learn_dart/lib/cascade_notation.dart
+++ first_learn_dart/lib/cascade_notation.dart
@@ -1,1 +1,13 @@
+class Player {
+  final String name;
+  int xp;
+  String team;
+
+  Player({required this.name, required this.xp, required this.team});
+
+  void sayhello() {
+    print("hello my nam is $name, my team is $team and they have $xp");
+  }
+}
+
 void main() {}
